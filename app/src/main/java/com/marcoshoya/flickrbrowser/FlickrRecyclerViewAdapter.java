14f4576a811556c32f11e1ce6c935a8da48aa663
marcoshoya <marcoshoya@gmail.com>
2017-03-20 22:54:03 -0300

    Flickr adapter

--- app/src/main/java/com/marcoshoya/flickrbrowser/FlickrRecyclerViewAdapter.java
+++ app/src/main/java/com/marcoshoya/flickrbrowser/FlickrRecyclerViewAdapter.java
@@ -2,9 +2,14 @@ package com.marcoshoya.flickrbrowser;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -21,6 +26,43 @@ class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewA
     public FlickrRecyclerViewAdapter(Context context, List<Photo> photoList) {
         this.photoList = photoList;
         this.context = context;
+    }
+
+    @Override
+    public FlickrImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        Log.d(TAG, "onCreateViewHolder: new view requested");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.browse, parent, false);
+
+        return new FlickrImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(FlickrImageViewHolder holder, int position) {
+        // called by the layout manager when it wants new data in an existing row
+
+        Photo item = photoList.get(position);
+        Picasso.with(context).load(item.getImage())
+                .error(R.drawable.placeholder)
+                .placeholder(R.drawable.placeholder)
+                .into(holder.thumbnail);
+
+        holder.title.setText(item.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount: called");
+        return ((photoList != null) && (photoList.size() != 0) ? photoList.size() : 0);
+    }
+
+    void loadNewData(List<Photo> newPhotos) {
+        this.photoList = newPhotos;
+        notifyDataSetChanged();
+    }
+
+    public Photo getPhoto(int idx) {
+        return ((photoList != null) && (photoList.size() != 0) ? photoList.get(idx) : null);
     }
 
     static class FlickrImageViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/marcoshoya/flickrbrowser/MainActivity.java
+++ app/src/main/java/com/marcoshoya/flickrbrowser/MainActivity.java
@@ -2,11 +2,14 @@ package com.marcoshoya.flickrbrowser;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements GetFlickrJsonData.OnDataAvailable {
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements GetFlickrJsonData
     private static final String TAG = "MainActivity";
 
     public static final String URL = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1";
+    private FlickrRecyclerViewAdapter flickrRecyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,12 @@ public class MainActivity extends AppCompatActivity implements GetFlickrJsonData
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        flickrRecyclerViewAdapter = new FlickrRecyclerViewAdapter(this, new ArrayList<Photo>());
+        recyclerView.setAdapter(flickrRecyclerViewAdapter);
 
         //GetRawData getRawData = new GetRawData(this);
         //getRawData.execute(URL);
@@ -59,5 +69,10 @@ public class MainActivity extends AppCompatActivity implements GetFlickrJsonData
     @Override
     public void onDataAvailable(List<Photo> data , DownloadStatus status) {
         Log.d(TAG, "onDataAvailable: status: " + status);
+
+        if (status == DownloadStatus.OK) {
+            this.flickrRecyclerViewAdapter.loadNewData(data);
+        }
+
     }
 }
